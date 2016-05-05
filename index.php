9a7f4950772ea93a060dfafb93d4661704c205e8
Vie <yangchenzhong@gmail.com>
2016-05-05 08:37:46 +0800

    body

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<!DOCTYPE HTML><html><head><meta charset="utf-8" /><title>Vie</title></head>
+<!DOCTYPE HTML><html><head><meta charset="utf-8" /><title>Vie</title></head><body>
 <?php
-  echo "Hello World! Test PHP, by Vie.";
+  echo "Hello World! Test PHP, by Vie. 20160505083700";
 ?>
+</body>
