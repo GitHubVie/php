db10ba1a3701ca12835526039000cdf6692649d2
chenzhong <chenzhong@iscas.ac.cn>
2016-05-27 14:26:21 +0800

    php

--- index.php
+++ index.php
@@ -6,21 +6,25 @@
 </head>
 
 <body>
-	<form class="form-horizontal" role="form" method="post" action="string.php">
-	<div class="form-group">
-		<label for="string" class="col-sm-5 control-label">String json_decode</label>
-		<div class="col-sm-7">
-			<input type="text" class="form-control" name="string" placeholder="mantis" value="<?php echo plugin_config_get('queue_name'); ?>">
+	<div class="container">      
+		<div class="col-sm-6 col-sm-offset-3">
+			<form class="form-horizontal" role="form" method="post" action="string.php">
+			<div class="form-group">
+				<label for="string" class="col-sm-5 control-label">String json_decode</label>
+				<div class="col-sm-7">
+					<input type="text" class="form-control" name="string" placeholder="string" value="string">
+				</div>
+			</div>
+						
+			<div class="form-group">
+				<div class="col-sm-offset-6 col-sm-8">
+					<?php if(isset($info)) { ?>
+					<p style="color:blue;"><?php echo $info; ?></p>
+					<?php } ?>
+					<input id="submit" name="submit" type="submit" value="submit" class="btn btn-primary">
+				</div>
+			</div>
+			</form>
 		</div>
 	</div>
-				
-	<div class="form-group">
-		<div class="col-sm-offset-6 col-sm-8">
-			<?php if(isset($info)) { ?>
-			<p style="color:blue;"><?php echo $info; ?></p>
-			<?php } ?>
-			<input id="submit" name="submit" type="submit" value="submit" class="btn btn-primary">
-		</div>
-	</div>
-	</form>
 </body>
